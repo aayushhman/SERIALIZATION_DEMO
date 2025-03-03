4ee3da21f5f58817300c822119426b40109abd8a
aayushhman <aayushmandal696@gmail.com>
2025-03-03 10:15:19 +0545

    serialization project

---
+++ SERIALIZATION_DEMO/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERIALIZATION_DEMO
+{
+    [Serializable]
+    class Employee
+    {
+        public int Id;
+        public string Name;
+        public Employee(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+                
+        }
+    }
+}
--- SERIALIZATION_DEMO/Program.cs
+++ SERIALIZATION_DEMO/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace SERIALIZATION_DEMO
 {
@@ -10,6 +12,14 @@ namespace SERIALIZATION_DEMO
     {
         static void Main(string[] args)
         {
+            string path = @"D:\Aayush new\sample.png";
+            Employee emp = new Employee(123,"Aayush");
+            FileStream stream = new FileStream(path,FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, emp);
+            stream.Close();
+            Console.WriteLine("FIle created sucessfully..");
+            Console.ReadLine();
         }
     }
 }
